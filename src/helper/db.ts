d3e17981d9e937ee7a93288681e720282f61601b
indumathi-ravi <indumathi123indumathi@gmail.com>
2025-05-28 10:22:12 +0530

    update db

--- src/helper/db.ts
+++ src/helper/db.ts
@@ -3,12 +3,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// const pool = new Pool({
+//   user: process.env.DB_USER || "postgres",
+//   host: process.env.DB_HOST || "localhost",
+//   database: process.env.DB_NAME || "sports-app-db",
+//   password: process.env.DB_PASSWORD || "1234",
+//   port: Number(process.env.DB_PORT) || 5432,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "sports-app-db",
-  password: process.env.DB_PASSWORD || "1234",
-  port: Number(process.env.DB_PORT) || 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  max: 15,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
 });
 
 export const executeQuery = async (
